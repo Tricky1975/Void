8302a03033810f7265581a2a9c30ece4ea9046ee
Jeroen P. Broks <facebook@tbbs.nl>
2019-11-05 23:50:42 +0100

    Register

--- Lex/LexNIL.cs
+++ Lex/LexNIL.cs
@@ -23,6 +23,7 @@
 // 
 // Version: 19.11.05
 // EndLic
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,7 @@ namespace Void.Lex {
 
         public LexNIL() {
             KeyWords = "and break do else elseif end false for function if in local nil not or repeat return then true until while void number int table string bool boolean class CONSTRUCTOR NEW userdata switch case private global NIL static get set forever #use #localuse #globaluse readonly var self default module DESTRUCTOR delegate NIL new group link with extends abstract final quickmeta".Split(' ');
+            Reg["nil"] = this;
         }
     }
 }
